2433bd029f358e79d6b8a94eb7a752a27805ddff
Victormarzo <victor.marzo.monteiro@gmail.com>
2022-06-18 08:58:00 -0300

    logica pronta

--- script.js
+++ script.js
@@ -1,14 +1,50 @@
 let quantidade;
 let front=["./images/coquinha.jpeg","./images/cha.jpeg","./images/frango.jpeg","./images/macarrao.jpeg","./images/mousse.jpeg","./images/parmegiana.jpeg","./images/petit.jpeg",];
 let frontParrot=["./images/bobrossparrot.gif","./images/explodyparrot.gif","./images/fiestaparrot.gif","./images/metalparrot.gif","./images/revertitparrot.gif","./images/tripletsparrot.gif","./images/unicornparrot.gif",]
+let parrot=["bobross","explody","fiesta","metal","revertit","triplets","unicorn"]
 let arrayParrot=[];
+let arraycompara=[];
 let cartaTemplate;
 function comparador() { 
 	return Math.random() - 0.5; 
 }
 function giraCarta(elemento){
+    
+    let primeiraCarta = document.querySelector(".primeira");
+    
+    if(primeiraCarta !== null){
+        if(primeiraCarta!==elemento){
+            elemento.classList.add("segunda");
+            elemento.classList.add("flip");
+            
+            
+        }else{
+            alert('Escolha cartas diferentes');
+        }
+        
+    }
+    else {
+    elemento.classList.add("primeira");
     elemento.classList.add("flip");
+   
 }
+setTimeout(comparaCarta, 2000);
+}
+function comparaCarta(){
+    let primeiraCarta = document.querySelector(".primeira");
+    let segundaCarta = document.querySelector(".segunda");
+    if(primeiraCarta.querySelector(".front").src ===segundaCarta.querySelector(".front").src){
+        primeiraCarta.removeAttribute("onclick");
+        segundaCarta.removeAttribute("onclick");
+    }else{
+        primeiraCarta.classList.remove("flip");
+        segundaCarta.classList.remove("flip");
+        console.log("Deu");
+    }
+    primeiraCarta.classList.remove("primeira");
+    segundaCarta.classList.remove("segunda");
+}
+
 function quantidadeCarta(){
         quantidade=prompt("Quantas cartas você quer jogar? Min:4 Max:14")
     while((quantidade<4)||(quantidade>14)||(quantidade%2!=0)||(isNaN(quantidade)==true)){
@@ -21,8 +57,8 @@ function quantidadeCarta(){
         }
         cartaTemplate = 
         `
-        <div class="cartaMemoria" onclick="giraCarta(this)">
-        <img class="front"src=${front[contador]}>
+        <div class="cartaMemoria ${parrot[contador]}" onclick="giraCarta(this)">
+        <img class="front "src=${frontParrot[contador]}>
         <img class="back" src="./images/front.png" >
         
         </div>
@@ -30,7 +66,7 @@ function quantidadeCarta(){
         arrayParrot.push(cartaTemplate);
            
     }
-    arrayParrot.sort(comparador);
+    //arrayParrot.sort(comparador);
     for(let i=0;i<arrayParrot.length;i++){
         document.querySelector(".tabuleiro").innerHTML += arrayParrot[i];
     }
